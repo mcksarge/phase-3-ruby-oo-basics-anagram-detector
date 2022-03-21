495484704118653c240f269a18bd7c25127cc652
mcksarge <higgins.fred.s@gmail.com>
2022-03-21 14:14:10 -0700

    commit tree 1f3b43e4cee80cc17291371e15e4e0b430cf812e

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+class Anagram
+
+
+    attr_reader :word
+
+    def initialize(word)
+        @word = word
+    end
+
+
+    def match(word)
+        new_word = word
+        old_word = @word
+        puts new_word
+        
+        new_word.select{|word| word.chars.sort == old_word.chars.sort}
+    end
+
+end
